d913b367ef6905b378d24b30a4de7c8a68d7980c
PratyushPaul2 <74598606+PratyushPaul2@users.noreply.github.com>
2021-01-24 20:17:58 +0530

    Update mango.js

--- mango.js
+++ mango.js
@@ -1,18 +1,18 @@
 class mango extends BaseClass {
-    constructor(x, y){
+  constructor(x, y){
 
-      super(x,y,50,70);
-      this.image = loadImage("Images/mango.png");
-      this.visibility=255;
-    }
-     display(){
-  if (this.body.speed<3) {
-    super.display();
-  } else {
-    push();
-    //this.visibility=this.visibility-5;
-    image(this.image,this.body.position.x,this.body.position.y,50,50);
-    pop();
+    super(x,y,50,70);
+    this.image = loadImage("Images/mango.png");
+    this.visibility=255;
   }
-     }
-  };
+   display(){
+if (this.body.speed<3) {
+  super.display();
+} else {
+  push();
+  //this.visibility=this.visibility-5;
+  image(this.image,this.body.position.x,this.body.position.y,70,90);
+  pop();
+}
+   }
+};
