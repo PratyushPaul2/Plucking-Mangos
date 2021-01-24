5c9d992527f68157a32a4925565beb92a3eb84fd
PratyushPaul2 <74598606+PratyushPaul2@users.noreply.github.com>
2021-01-24 20:07:48 +0530

    Update mango.js

--- mango.js
+++ mango.js
@@ -9,10 +9,8 @@ class mango extends BaseClass {
   if (this.body.speed<3) {
     super.display();
   } else {
-    World.remove(world,this.body);
     push();
-    this.visibility=this.visibility-5;
-    tint(255,this.visibility);
+    //this.visibility=this.visibility-5;
     image(this.image,this.body.position.x,this.body.position.y,50,50);
     pop();
   }
